5d0c904bda7023ae5d8e62863d8a32bbd7078366
MasahiroLittleforest <masahiro.littleforest@gmail.com>
2020-04-05 17:55:15 +0900

    added app info to LicensePage

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:package_info/package_info.dart';
 
 import './neumorphic_container.dart';
 
@@ -37,8 +38,23 @@ class AppDrawer extends StatelessWidget {
                   ),
                 ),
               ),
-              onTap: () {
-                showLicensePage(context: context);
+              onTap: () async {
+                final PackageInfo packageInfo =
+                    await PackageInfo.fromPlatform();
+                final String appName = packageInfo.appName;
+                final String version = packageInfo.version;
+                final double iconSize = 180;
+                showLicensePage(
+                  context: context,
+                  applicationName: appName,
+                  applicationVersion: version,
+                  applicationIcon: Image.asset(
+                    'assets/icon/icon_512_iphone.png',
+                    width: iconSize,
+                    height: iconSize,
+                    fit: BoxFit.scaleDown,
+                  ),
+                );
               },
             ),
             const SizedBox(height: 20),
